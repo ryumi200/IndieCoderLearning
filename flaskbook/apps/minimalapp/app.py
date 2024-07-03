1cf2167dd8d528afa5fcb115eeacda3b6d91cfdf
ryumi200 <miyake200cm@icloud.com>
2024-07-03 18:30:41 +0900

    add new directory "flaskbook"

---
+++ flaskbook/apps/minimalapp/app.py
@@ -0,0 +1,30 @@
+# Flaskクラスをimportする
+from flask import Flask, render_template, url_for, current_app, g, request
+
+# Flaskクラスをインスタンス化する
+app = Flask(__name__)
+
+
+# URLと実行する関数をマッピングする
+@app.route("/")
+def index():
+    return "Hello, Flaskbook!"
+
+
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello, {name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    print(url_for("index"))
+    print(url_for("hello-endpoint", name="world"))
+    print(url_for("show_name", name="ichiro", page="1"))
+
+with app.test_request_context("/users?updated=true"):
+    print(request.args.get("updated"))
