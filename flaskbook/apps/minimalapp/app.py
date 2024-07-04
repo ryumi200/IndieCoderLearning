82493a52dd9f7641090f4bbb08b48ad942b2c8aa
ryumi200 <miyake200cm@icloud.com>
2024-07-04 14:45:39 +0900

    add chapter1

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,8 +1,40 @@
+# loggingをimportする
+import logging
+import os
+
+# type: ignore
+from flask_debugtoolbar import DebugToolbarExtension
+
 # Flaskクラスをimportする
-from flask import Flask, render_template, url_for, current_app, g, request
+from email_validator import validate_email, EmailNotValidError
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+    current_app,
+    g,
+    request,
+    redirect,
+    flash,
+    make_response,
+    session,
+)
+from flask_mail import Mail, Message
+
 
 # Flaskクラスをインスタンス化する
 app = Flask(__name__)
+
+# SECRET_KEYを追加する
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
+
+# ログレベルを設定する
+app.logger.setLevel(logging.DEBUG)
+
+# リダイレクトを中断しないようにする
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+# DebugToolbarExtensionにアプリケーションをセットする
+toolbar = DebugToolbarExtension(app)
 
 
 # URLと実行する関数をマッピングする
@@ -11,7 +43,7 @@ def index():
     return "Hello, Flaskbook!"
 
 
-@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+@app.route("/hello/<name>", methods=["GET"], endpoint="hello-endpoint")
 def hello(name):
     return f"Hello, {name}!"
 
@@ -26,5 +58,108 @@ with app.test_request_context():
     print(url_for("hello-endpoint", name="world"))
     print(url_for("show_name", name="ichiro", page="1"))
 
+
+# アプリケーションコンテキストを取得してスタックへpushする
+ctx = app.app_context()
+ctx.push()
+
+# current_appにアクセスが可能になる
+print(current_app.name)
+
+# グローバルなテンポラリ領域に値を設定する
+g.connection = "connection"
+print(g.connection)
+
 with app.test_request_context("/users?updated=true"):
     print(request.args.get("updated"))
+
+
+@app.route("/contact")
+def contact():
+    # レスポンスオブジェクトを取得する
+    response = make_response(render_template("contact.html"))
+
+    # クッキーを設定する
+    response.set_cookie("flaskbook key", "flaskbook value")
+
+    # セッションを設定する
+    session["username"] = "ichiro"
+
+    # レスポンスオブジェクトを返す
+    return response
+
+    return render_template("contact.html")
+
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        # form属性を使ってフォームの値を取得する
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+        # 入力チェック
+        is_valid = True
+
+        if not username:
+            flash("ユーザ名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+
+        if not description:
+            flash("問い合わせ内容は必須です")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
+        # メールを送る
+        send_email(
+            email,
+            "問い合わせありがとうございました",
+            "contact_mail",
+            username=username,
+            description=description,
+        )
+
+        # 問い合わせ完了エンドポイントへリダイレクトする
+        flash(
+            "問い合わせ内容はメールにて送信しました。問い合わせありがとうございます。"
+        )
+
+        # contactエンドポイントへリダイレクトする
+        return redirect(url_for("contact_complete"))
+    return render_template("contact_complete.html")
+
+
+app.logger.critical("fatal error")
+app.logger.error("error")
+app.logger.warning("warning")
+app.logger.info("info")
+app.logger.debug("debug")
+
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+# flask-mail拡張を登録する
+mail = Mail(app)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(subject, recipients=[to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
