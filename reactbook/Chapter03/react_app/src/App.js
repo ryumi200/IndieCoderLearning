e1f04dca6d00d2365a85ebb190660ea217546c34
ryumi200 <miyake200cm@icloud.com>
2024-07-29 16:10:54 +0900

    progress in Jul 29.

--- reactbook/Chapter03/react_app/src/App.js
+++ reactbook/Chapter03/react_app/src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import Rect from './Rect'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  data = [
+    "This is list sample.",
+    "これはリストのサンプルです。",
+    "配列をリストに変換します。",
+    "追加で文章を書いても",
+    "このようにリストを作ってくれます"
+  ]
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: this.data
+    }
+  }
+
+  render() {
+    return <div>
+      <h1 className='bg-primary text-white display-4'>React</h1>
+      <div className='contaienr'>
+        <p className='subtitle'>Show List.</p>
+        <List title="サンプル・リスト" data={this.data} />
+      </div>
     </div>
-  );
+  }
 }
 
-export default App;
+class List extends Component {
+  number = 1
+
+  render() {
+    let data = this.props.data
+    return (
+      <div>
+        <p className='h5 text-center'>{this.props.title}</p>
+        <ul className='list-group'>
+          {data.map((item, key) =>
+            <li className='list-group-item' key={key}>
+              <Item number={key + 1} value={item} />
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+class Item extends Component {
+  itm = {
+    fontSize: "16pt",
+    color: "#00f",
+    textDecoration: "underline",
+    textDecorationColor: "#ddf"
+  }
+
+  num = {
+    fontWeight: "bold",
+    color: "red"
+  }
+
+  render() {
+    return (
+      <p style={this.itm}>
+        <span style={this.num}>
+          [{this.props.number}]&nbsp;
+        </span>
+        {this.props.value}
+      </p>
+    )
+  }
+}
+export default App
