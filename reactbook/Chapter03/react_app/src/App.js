7b4d2c2266d8194b86fa24b11bc46afc8825c6b9
ryumi200 <miyake200cm@icloud.com>
2024-08-04 02:35:50 +0900

    finished chapter04

--- reactbook/Chapter03/react_app/src/App.js
+++ reactbook/Chapter03/react_app/src/App.js
@@ -1,41 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Rect from './Rect';
-import React, { Component, useState } from 'react';
+import MemoPage from './memo/MemoPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [flag, setFlag] = useState(false)
-  const clickFunc = () => {
-    setCount(count + 1)
-  }
-  const changeFlag = (e) => {
-    setFlag(e.target.checked)
-  }
-
   return (
     <div>
       <h1 className='bg-primary text-white display-4'>React</h1>
-      <div className='container'>
-        <h4 className='my-3'>Hooks sample</h4>
-        {flag ?
-          <div className='alert alert-primary text-center'>
-            <p className='h5 mb-3'>click: {count} times!</p>
-            <div><button className='btn btn-primary' onClick={clickFunc}>Click me</button></div>
-          </div>
-          :
-          <div className='card p-3 border-primary text-center'>
-            <p className='h5 mb-3 text-left text-primary'>click: {count} times!</p>
-            <div><button className='btn btn-primary' onClick={clickFunc}>Click me</button></div>
-          </div>
-        }
-        <div className='form-group h6 text-center pt-3'>
-          <input type="checkbox" className='form-check-input' id='check1' onChange={changeFlag} />
-          <label className='form-check-label' htmlFor="check1">Change form style.</label>
-        </div>
+      <div className='contaienr'>
+        <h4 className='my-3'>Memo.</h4>
+        <MemoPage />
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
