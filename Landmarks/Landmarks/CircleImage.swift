d492907c79d299a984a4749a9595bca61ce62f7c
ryumi200 <miyake200cm@icloud.com>
2024-09-02 17:37:28 +0900

    add swiftUI tutorial 1-1

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Ryu Miyake on 2024/09/02.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ryu Miyake on 2024/09/02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
