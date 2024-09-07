03e9762947573d0cc3530cae67e1cd5b1eb1d36b
ryumi200 <miyake200cm@icloud.com>
2024-09-07 20:04:16 +0900

    finished SwiftUI tutorial

--- SwiftUI/4-3StartingPoint/Landmarks/Views/Landmarks/LandmarkList.swift
+++ SwiftUI/4-3StartingPoint/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -10,11 +10,28 @@ import SwiftUI
 struct LandmarkList: View {
     @Environment(ModelData.self) var modelData
     @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
+    
+    enum FilterCategory: String, CaseIterable, Identifiable {
+            case all = "All"
+            case lakes = "Lakes"
+            case rivers = "Rivers"
+            case mountains = "Mountains"
 
+
+            var id: FilterCategory { self }
+        }
+    
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
+                && (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
+    }
+    
+    var title: String {
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
     }
 
     var body: some View {
@@ -33,7 +50,25 @@ struct LandmarkList: View {
                 }
             }
             .animation(.default, value: filteredLandmarks)
-            .navigationTitle("Landmarks")
+            .navigationTitle(title)
+            .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
+                            }
+                        }
+                        
+                        Toggle(isOn: $showFavoritesOnly) {
+                            Label("Favorites only", systemImage: "star.fill")
+                        }
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
         } detail: {
             Text("Select a Landmark")
         }
---
+++ SwiftUI/4-3StartingPoint/WatchLandmarks Watch App/LandmarkList.swift
@@ -0,0 +1,52 @@
+//
+//  LandmarkList.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Ryu Miyake on 2024/09/07.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .animation(.default, value: filteredLandmarks)
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+
+#Preview {
+    LandmarkList()
+        .environment(ModelData())
+}
